e511be433dae9c0c3bb25dded9a4f02cb6755ea7
zikoelj <zakariaelj24@gmail.com>
2024-01-03 17:29:18 -0800

    good

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        last = abs(number) % 10
+        print(f"{last}", end="")
+        return last
+    else:
+        last = number % 10
+        print(f"{last}", end="")
+        return last
