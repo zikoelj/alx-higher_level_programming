0ecd46895bfb31bc809f5ece9093173fc3f12b85
zikoelj <zakariaelj24@gmail.com>
2024-02-05 18:17:20 -0800

    good with oop

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""defines a square bassed on 1-square.py"""
+
+
+class Square:
+    """Square"""
+
+    def __init__(self, size=0):
+        """Initialize the data"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+         """return the current square area"""
+         return (self.__size * self.__size)
