a35f35e2f2db4d4da9280498158e11e059f41ffb
zikoelj <zakariaelj24@gmail.com>
2024-03-12 16:06:25 -0700

    sql

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Write a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
+-- id = 89 and  name = Best School
+INSERT INTO first_table VALUES (89,"Best School");
