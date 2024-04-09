943b985bbd8f718cbac27371ec497b17da799536
zikoelj <zakariaelj24@gmail.com>
2024-04-08 18:45:33 -0700

    js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// calculate the number of arguments passed
+const numberOfArguments = process.argv.length - 2;
+
+if (numberOfArguments === 0) {
+  console.log('No argument');
+} else if (numberOfArguments === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
