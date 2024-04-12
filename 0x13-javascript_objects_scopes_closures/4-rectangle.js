4af9e552aad0ea5efbb29cf2235ef6bbdeb11c1f
zikoelj <zakariaelj24@gmail.com>
2024-04-11 19:51:20 -0700

    js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rec = '';
+      for (let j = 0; j < this.width; j++) {
+        rec += 'X';
+      }
+      console.log(rec);
+    }
+  }
+
+  rotate () {
+    const w = this.width;
+    this.width = this.height;
+    this.height = w;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
