0d25d80daca46716a5fa42b596c801363842aa1c
zikoelj <zakariaelj24@gmail.com>
2024-04-29 07:36:51 -0700

    networking in python

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    content = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
